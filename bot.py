e7b4774552210f912255aa7971d4562d6f088153
brockar <martinnguzman.mg@gmail.com>
2021-12-11 23:53:56 -0300

    Botones 1

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def input_text(update, context):
 
 if __name__== '__main__':
 
-        updater = Updater(token='', use_context=True)
+        updater = Updater(token='a', use_context=True)
         dp= updater.dispatcher
 
         dp.add_handler(CommandHandler('start', start))
